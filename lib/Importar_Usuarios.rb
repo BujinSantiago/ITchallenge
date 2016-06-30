55a7b43c7c8dbc0f563995d9f3b6a68649879812
scalgaro <s.calgaro.martin@gmail.com>
2016-06-30 18:10:58 -0300

    Reestructuracion

--- lib/Importar_Usuarios.rb
+++ lib/Importar_Usuarios.rb
@@ -2,7 +2,6 @@
 
 # NOTAS:
 # Se omiten los acentos en la totalidad del codigo y comentarios.
-# Encriptado de credenciales :simple_tls esta comentado por que me traia probemas con el ssl de la maquina virtual. (Perdi mucho tiempo tratando de solucionarlo)
 # Espero que les guste!
 
 
@@ -21,12 +20,20 @@ ldapbase = "dc=dnsdtest,dc=com"
 # Dependiendo del tipo de encriptacion que utiliza el servidor
 # se debería cambiar el siguiente parametro
 #  :encryption => :simple_tls,
+tls_encryption = false
 # Usuario de gmail, para este ejemplo, con la opcion de "aplicaciones menos seguras" activada.
 # Este sera el remitente de los mails informativos
 # Este que viene seteado funciona:
 gmailuser = "itchallengetest@gmail.com"
 # Contraseña del usuario de gmail.
 gmailpasswd = "quinn1234"
+# Esta variable define si se va a enviar un mail en caso de que el usuario no haya sido creado.
+send_failure_mail = true
+# Esta variable define si los usuarios van a ser creados con password,
+# dependiendo del tipo de conexion a LDAP (tls, ssl), crear usuarios con password
+# podria fallar.
+users_with_passwd = false
+
 
 #################################################
 
@@ -124,13 +131,13 @@ puts '********************'
 puts ' '
 
 puts 'Ingrese la direccion del archivo CSV a importar:'
-puts 'Ejemplo: CSVdummy/usuarios.csv'
+puts 'Ejemplo: ../test/usuarios_example.csv'
 puts ' '
-csvfile = gets.chomp
+csvfile = "../test/usuarios_example.csv"#gets.chomp
 puts ' '
 
 while (!File.file?(csvfile)) do
-  puts 'El path isngresado es incorrecto, vuelva a intentarlo.'
+  puts 'El path ingresado es incorrecto, vuelva a intentarlo.'
   puts ' '
   csvfile = gets.chomp
   puts ' '
@@ -142,48 +149,69 @@ hashed_data = csvarray.map { |d| d.to_hash }
 
 hashed_data.each { |data|
 
-  # Todo esto tendria que estar en una funcion aparte.
-
-  dn = "uid=test,ou=users,dc=dnsdtest,dc=com"
+  # Todo esto tendria que estar en una funcion aparte, mejorar mas adelante.
 
   first_name = data[:nombre]
   last_name = data[:apellido]
   username = first_name[0].downcase + last_name.downcase
   fullname = first_name + " " + last_name
   usermail = data[:email]
+  plain_passwd = " "
 
-  attrs = {
-    #:objectclass => ["top", "person"],
-    :cn => fullname,
-    :sn => last_name.capitalize,
-    #:givenname => first_name.capitalize,
-    #:displayname => fullname,
-    #:name => fullname,
-    #:samaccountname => username,
-    #:unicodePwd => '"password"'.encode("utf-16")
-  }
-  ldap.add(dn: dn, attributes: attrs)
+  dn = "uid="+username+",ou=users,dc=dnsdtest,dc=com"
 
+  # Dependiendo de  si va a tener password o no, armo los atributos
+  if users_with_passwd then
+
+    # Creo una passwd
+    plain_passwd = ""; 8.times{plain_passwd << ((rand(2)==1?65:97) + rand(25)).chr}
+    # Codifico la passwd con md5
+    coded_password = Net::LDAP::Password.generate(:md5, plain_passwd)
+
+    # Esto no lo probe, dado que mi conexion de OpenSSL no me permitia
+    # autentificarme en el LDAP
+
+    attrs = {
+      :objectclass => ["User"],
+      :cn => username,
+      :sn => last_name.capitalize,
+      :givenname => first_name.capitalize,
+      :unicodePwd => coded_password
+    }
+
+  else
+    attrs = {
+      :objectclass => ["User"],
+      :cn => username,
+      :sn => last_name.capitalize,
+      :givenname => first_name.capitalize,
+      }
+  end
+
+  # Creo el usuario
+  ldap.add(:dn => dn, :attributes => attrs)
+
+  # Envio el mail correspondiente
   if ldap.get_operation_result.code != 0
     puts "Failed to add user #{fullname}"
 
+    if(send_failure_mail) then
     Mail.deliver do
       to data[:email]
       from 'itchallengetest@gmail.com'
       subject 'No se ha podido crear su usuario.'
-      body 'No se ha podido crear su nuevo usuario para acceder a ' + ldaphost + ', ' + username
+      body 'No se ha podido crear su nuevo usuario para acceder a ' + ldaphost + ', ' + username + ' (Contraseña: ' + plain_passwd + ')'
     end
+  end
 
   else
     puts "Added user #{fullname}"
-
-    Mail.deliver do
-      to data[:email]
-      from 'itchallengetest@gmail.com'
-      subject 'Su nuevo usuario a sido creado.'
-      body 'Su nuevo usuario para acceder a ' + ldaphost + 'es: ' + username
-    end
-
+      Mail.deliver do
+        to data[:email]
+        from 'itchallengetest@gmail.com'
+        subject 'Su nuevo usuario a sido creado.'
+        body 'Su nuevo usuario para acceder a ' + ldaphost + 'es: ' + username+ ' y su contraseña es: '+ plain_passwd
+      end
   end
 }
 
